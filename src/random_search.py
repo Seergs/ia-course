85e7d24e4e2ac67273061acb653cd8c46d51d39b
Sergio <S.suarerez@gmail.com>
2020-09-29 12:59:18 -0500

    :heavy_plus_sign: Complete desc gradient

--- src/random_search.py
+++ src/random_search.py
@@ -6,16 +6,18 @@ from random import random
 from mpl_toolkits import mplot3d
 import time
 
-
 def f1(x, y):
     return x * (np.e ** (-(x ** 2) - y ** 2))
+
+def get_gradient_f1(x,y):
+    pass
 
 
 def f2(x1, x2):
     return (x1 - 2) ** 2 + (x2 - 2) ** 2
 
 
-def get_gradient(value):
+def get_gradient_f2(value):
     return 2 * (value - 2)
 
 
@@ -57,7 +59,6 @@ class RandomSearch:
                 self.x_best, self.y_best, self.f_best
             )
         )
-        self.__plot_surface()
         plt.show()
 
     def search_lite(self):
@@ -85,16 +86,14 @@ class RandomSearch:
         self.__plot_point(self.x_best, self.y_best, "go")
         plt.pause(0.005)
 
-    def __plot_surface(self):
+    def plot_surface(self):
         plt.figure()
-        mycmap = plt.get_cmap("gist_earth")
         ax = plt.axes(projection="3d")
-        ax.plot_surface(self.X, self.Y, self.Z, cmap=mycmap)
-        ax.plot(self.x_best, self.y_best, self.f_best, "ro", markersize="12")
-        ax.view_init(elev=8, azim=-14)
+        ax.plot_surface(self.X, self.Y, self.Z)
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_zlabel("z")
+        plt.show()
 
     def __plot_point(self, x, y, marker):
         if marker == "go":
@@ -106,17 +105,15 @@ class RandomSearch:
 
 
 class DescGradient:
-    def __init__(self, f, gradient_func, x0, y0, xl=-10, xu=10, yl=-10, yu=10):
+    def __init__(self, f, gradient_func, xl=-10, xu=10, yl=-10, yu=10):
         self.f = f
         self.xl = xl
         self.xu = xu
         self.yl = yl
         self.yu = yu
-        self.x0 = x0
-        self.y0 = y0
+        
         self.h = 0.1
-        self.xi = self.x0
-        self.yi = self.y0
+        
         self.gradient_func = gradient_func
 
         self.x_range = np.arange(xl, xu, 0.1)
@@ -124,7 +121,7 @@ class DescGradient:
         self.X, self.Y = np.meshgrid(self.x_range, self.y_range)
         self.Z = self.f(self.X, self.Y)
 
-    def plot(self):
+    def plot_surface(self):
         plt.figure()
         ax = plt.axes(projection="3d")
         ax.plot_surface(self.X, self.Y, self.Z)
@@ -133,7 +130,11 @@ class DescGradient:
         ax.set_zlabel("z")
         plt.show()
 
-    def search(self):
+    def search(self, x0, y0):
+        self.x0 = x0
+        self.y0 = y0
+        self.xi = self.x0
+        self.yi = self.y0
         for i in range(100):
             self.x_next = self.xi - self.h * get_gradient(self.xi)
             self.y_next = self.yi - self.h * get_gradient(self.yi)
@@ -147,8 +148,32 @@ class DescGradient:
         )
 
 
-# random_search = RandomSearch(f2, xl=1, xu=2, yl=1, yu=2)
-# random_search.search()
 
-gradient = DescGradient(f2, get_gradient, x0=1.8, y0=1.8, xl=1, xu=2, yl=1, yu=2)
-gradient.search()
+
+# --------Busqueda aleatoria-------
+
+# Primera funcion
+#random_search = RandomSearch(f1, xl=-2, xu=2, yl=-2, yu=2)
+#random_search.search()
+#random_search.plot_surface()
+
+# Segunda funcion
+#random_search = RandomSearch(f2, xl=1, xu=2, yl=1, yu=2)
+#random_search.search_lite()
+#random_search.plot_surface()
+
+
+
+# -------Gradiente---------
+
+
+# Primera funcion
+#gradient = DescGradient(f1, get_gradient_f1, xl=-2, xu=2, yl=-2, yu=2)
+#gradient.search(-0.5,0)
+#gradient.plot_surface()
+
+
+# Segunda funcion
+#gradient = DescGradient(f2, get_gradient_f2, xl=1, xu=2, yl=1, yu=2 )
+#gradient.search(1.8,1.8)
+#gradient.plot_surface()
