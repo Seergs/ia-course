bc375e822c4a9ebf33e0b7dfd6824bf3d24a8c90
Sergio <S.suarerez@gmail.com>
2020-10-14 18:31:01 -0500

    :nail_care: Format files

--- src/ga.py
+++ src/ga.py
@@ -1,7 +1,7 @@
 #   Sergio Suárez Álvarez
 #   217758497
 #   Seminario de solución de problemas de Inteligencia Artificial 1
-#   
+#
 #   Este programa busca el mínimo global de una función
 #   usando un algoritmo genético clásico.
 #   Para más información sobre el GA, visite https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3#:~:text=A%20genetic%20algorithm%20is%20a,offspring%20of%20the%20next%20generation
@@ -9,7 +9,7 @@
 #
 #
 
-'''
+"""
     Encuentra el mínimo global de una función
 
     Classes:
@@ -19,7 +19,7 @@
     Variables:
         ga
 
-'''
+"""
 
 
 # Librerías necesarias para el correcto funcionamiento
@@ -31,7 +31,7 @@ import operator
 
 # Función objetivo 2
 def f2(x1, x2):
-    '''
+    """
     Evalúa la función objetivo en el punto x,y
 
         Parameters:
@@ -41,16 +41,12 @@ def f2(x1, x2):
         Returns:
             z (Numeric): valor de la función en x,y
 
-    '''
+    """
 
     # Evaluamos la función objetivo en x,y
     z = (x1 - 2) ** 2 + (x2 - 2) ** 2
 
     return z
-
-
-
-
 
 
 # Clase para ejecutar el algoritmo genético
@@ -90,7 +86,7 @@ class GA:
         Lista de una dimensión que representa coordenadas de una matriz, para
         graficar el vector "z"
 
-    
+
     Methods
     -------
     plot():
@@ -109,11 +105,8 @@ class GA:
         Muta a los hijos con una probablidad baja
     """
 
-
-
-
     # Método ejecutado al instanciar la clase, recibe la función objetivo,
-    # el tamaño de la población (con valor por defecto de 100), 
+    # el tamaño de la población (con valor por defecto de 100),
     # y los límites tanto superior como inferior de "x" y "y"
     def __init__(self, f, pop_size=100, xl=-5, xu=5, yl=-5, yu=5):
         """
@@ -128,14 +121,14 @@ class GA:
             yl (Numeric): Límite inferior en "y"
             yu (Numeric): Límite superior en "y"
 
-        Returns: 
+        Returns:
             None
         """
 
         self.f = f
         self.pop_size = pop_size
 
-        #Inicializamos la dimension en 2
+        # Inicializamos la dimension en 2
         self.dimension = 2
         self.xl = xl
         self.xu = xu
@@ -146,8 +139,7 @@ class GA:
         # cada generación
         self.x = np.empty((self.dimension, pop_size))
 
-
-        #Inicializamos la lista de aptitudes como vacía
+        # Inicializamos la lista de aptitudes como vacía
         self.fitness = {}
 
         # Rango de valores entre el límite inferior y superior con una separación de 0.1
@@ -155,19 +147,11 @@ class GA:
         x_range = np.arange(self.xl, self.xu, 0.1)
         y_range = np.arange(self.yl, self.yu, 0.1)
 
-
-
         # Nuestros vectores "x" y "y"
         self.X, self.Y = np.meshgrid(x_range, y_range)
 
         # Calculamos nuestro vector "z" evaluando en la función objetivo "x" y "y"
         self.Z = self.f(self.X, self.Y)
-
-
-
-
-
-
 
     # Función que grafica el contour de la función objetivo en el rango especificado
     def plot(self):
@@ -176,19 +160,15 @@ class GA:
 
             Parameters:
                 None
-            
+
             Returns:
                 None
 
         """
 
-        # Se usa matplotlib para plotear nuestro contour        
+        # Se usa matplotlib para plotear nuestro contour
         plt.contour(self.X, self.Y, self.Z)
         plt.show()
-
-
-
-
 
     # Función inicial que comenza con la simulación del algoritmo genético
     def start(self, generations=1000):
@@ -202,10 +182,7 @@ class GA:
                 None
         """
 
-
-
         print("\n\nIniciando simulación..\nEsto puede llevar bastante tiempo\n")
-
 
         # Creamos dos vectores con numpy para nuestros límites.
         # Se usa este tipo de vector y no un array porque numpy nos permita
@@ -213,17 +190,14 @@ class GA:
         xl_vector = np.array([self.xl, self.yl])
         xu_vector = np.array([self.xu, self.yu])
 
-
         # 1. Llamamos a nuestro método encargado de inicializar la población
         self._initialize(xl_vector, xu_vector)
-
 
         # Iteramos en las generaciones
         for i in range(generations):
 
             # 2. Evaluamos los individuos de la población, calculando su aptitud
             self._evaluation()
-
 
             # 3. Se obtiene una matriz de hijos seleccionado a los padres mejor calificados
             childs = self._create_childs_by_selecting_parents()
@@ -236,12 +210,9 @@ class GA:
 
             # Se grafica en el contour el punto con mejor aptitud
             self._plot_contour(i)
-                
 
         # Esta línea nos permite tener la gráfica "abierta" mientras el algoritmo sigue corriendo
         plt.show()
-
-
 
         # Finalmente obtenemos nuestro vector con mejor aptitud
         x_best, y_best = self._get_best_fitness()
@@ -250,9 +221,6 @@ class GA:
                 round(x_best, 2), round(y_best, 2), round(self.f(x_best, y_best), 2)
             )
         )
-
-
-
 
     # Función que inicializa la población
     def _initialize(self, xl_vector, xu_vector):
@@ -263,10 +231,9 @@ class GA:
                 xl_vector (ndarray): Vector con los límites inferiores del rango
                 xu_vector (ndarray): Vector con los límites superiores del rango
 
-            Returns: 
+            Returns:
                 None
         """
-
 
         # Se itera creando individuos dentro del espacio de trabajo del espacio continuo
         for i in range(self.pop_size):
@@ -274,15 +241,12 @@ class GA:
                 (np.random.random(), np.random.random())
             )
 
-
-
-
     # Función que evalua cada individuo de la población, calculando su aptitud
     def _evaluation(self):
         """
         Evalúa los individuos de la población para calcular su aptitud
 
-            Parameters: 
+            Parameters:
                 None
 
             Returns:
@@ -299,31 +263,24 @@ class GA:
             else:
                 self.fitness[i] = 1 + abs(fx)
 
-
-
-
-
     # Función que crea a los hijos en base a la selección de dos padres
     def _create_childs_by_selecting_parents(self):
         """
-        Crea una nueva población de hijos seleccionado a los dos 
+        Crea una nueva población de hijos seleccionado a los dos
         padres aptos mediante ruleta
 
             Parameters:
                 None
 
-            Returns: 
+            Returns:
                 childs (ndarray): Matriz con los hijos creados
         """
-
 
         # Se crea una matriz vacía donde se colocarán los hijos
         childs = np.empty((self.dimension, self.pop_size))
 
-
         # Se itera de dos en dos en la población actual
         for i in range(0, self.pop_size, 2):
-
 
             # Se selecciona a los padres mediante ruleta
             first_parent = self._get_index_by_roulette_wheel_selection()
@@ -332,14 +289,12 @@ class GA:
             # realizar la comprobación de abajo
             second_parent = first_parent
 
-
             # El algortitmo dice que se seleccione a los padres n1 y n2 donde n1 != n2
             # por eso se realiza esta comprobación
             while second_parent == first_parent:
                 second_parent = self._get_index_by_roulette_wheel_selection()
 
-
-            # Se cruza a los padres para crear a los dos hijos 
+            # Se cruza a los padres para crear a los dos hijos
             first_child, second_child = self._mix(
                 self.x[:, first_parent], self.x[:, second_parent]
             )
@@ -350,10 +305,6 @@ class GA:
 
         return childs
 
-
-
-
-
     # Función para cruzar a los padres y crear a los hijos
     def _mix(self, first_parent, second_parent):
         """
@@ -361,7 +312,7 @@ class GA:
 
             Parameters:
                 first_parent (vector): El primer padre
-                second_parent (vector): El segundo padre 
+                second_parent (vector): El segundo padre
 
             Returns:
 
@@ -381,10 +332,6 @@ class GA:
 
         return first_child, second_child
 
-
-
-
-
     # Función que crea mutaciones en los hijos
     def _mutate(self, y, pm, xl, xu):
         """
@@ -396,7 +343,7 @@ class GA:
                 xl (vector): Vector de los límites inferiores del rango
                 xu (vector): Vector de los límites superiores del rango
 
-            Returns: 
+            Returns:
                 None
         """
 
@@ -409,11 +356,6 @@ class GA:
                 if np.random.random() < pm:
                     y[j, i] = xl[j] + (xu[j] - xl[j]) * np.random.random()
 
-
-
-
-
-
     # Función de utilidad para obtener un índice de los individuos de la población
     # utilizando el algoritmo de la selección por ruleta, que selecciona con mayor
     # probabilidad a los individuos con mejor aptitud
@@ -424,7 +366,7 @@ class GA:
 
             Parameters:
                 None
-            
+
             Returns:
                 i (Numeric): Índice del individuo de la población
                 /pop_size (Numeric): Si no se encuentra un individuo se devuelve el último elemento
@@ -444,10 +386,6 @@ class GA:
 
         return self.pop_size
 
-
-
-
-
     # Función que retorn el elemento con mejor aptitud de la población
     def _get_best_fitness(self):
         """
@@ -464,16 +402,12 @@ class GA:
         # para obtener el key que tenga un value más alto
         key = max(self.fitness.items(), key=operator.itemgetter(1))[0]
 
-        x = self.x[0,key]
-        y = self.x[1,key]
+        x = self.x[0, key]
+        y = self.x[1, key]
 
-        return x,y
+        return x, y
 
-
-
-
-
-    # Función que plotea tanto la gráfica de la función objetivo 
+    # Función que plotea tanto la gráfica de la función objetivo
     # como el punto con mejor aptitud en cada generación
     # Esta función se ejecuta en cada generación
     def _plot_contour(self, i):
@@ -485,9 +419,8 @@ class GA:
 
             Returns:
                 None
-            
-        """
 
+        """
 
         # Limpíamos la gráfica para eliminar el marcador de la generación pasada
         plt.clf()
@@ -506,8 +439,6 @@ class GA:
 
         # Se usa una pausa para que se actualice la gráfica
         plt.pause(0.005)
-
-
 
     # Función que plotea un punto en la gráfica
     def _plot_point(self, x, y, i):
@@ -533,11 +464,6 @@ class GA:
 
         # Se plotea el punto
         plt.plot(x, y, "ro")
-
-
-
-
-
 
 
 # Se instancia la clase con la función objetivo y una población de 200
